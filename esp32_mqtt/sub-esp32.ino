e551605a1e30ed7d00a4a28168d86dabe2a54390
Douglas Maia <douglas.maia.santos@itec.ufpa.br>
2023-07-19 23:37:01 -0300

    first version of mqtt pub and sub codes

---
+++ esp32_mqtt/sub-esp32.ino
@@ -0,0 +1,92 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+// WiFi configuration
+char* ssid     = "your_network_id";
+char* password = "your_network_password";
+// MQTT configuration
+char* broker   = "your_mqtt_broker_id";
+int mqtt_port  = 1883;
+char* mqtt_username = "your_mqtt_username";
+char* mqtt_password = "your_mqtt_password";
+
+char* topic = "your_topic";
+
+// Create a object to set ESP-32 to mqtt client
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  setup_mqtt();
+
+  client.subscribe(topic);
+  print("Subscribe to topic: ");
+  print(topic);
+}
+
+void loop() {
+  client.loop();
+
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Topic: ");
+  Serial.print(topic);
+  char message[length + 1];
+  for (int i = 0; i < length; i++) {
+    message[i] = (char)payload[i];
+  }
+  message[length] = '\0';
+  Serial.print("Message arrived: ");
+  Serial.println(message);
+}
+
+void setup_mqtt() {
+  delay(10);
+  client.setServer(broker, mqtt_port);
+  client.setCallback(callback);
+
+  while (!client.connected()){
+    delay(100);
+    Serial.println();
+    Serial.print("Connecting to MQTT Broker");
+    if (client.connect("ESP32Client", mqtt_username, mqtt_password)){
+      Serial.println("Connected to Broker!");
+    }
+    else {
+      Serial.println("Failed to connect to Broker!");
+      Serial.print("Broker state: ");
+      Serial.print(client.state());
+      Serial.println();
+      Serial.println("Retrying in 2 seconds...");
+      delay(2000);
+    }
+  }
+}
+
+void setup_wifi() {
+  delay(100);
+  // serial feedback
+  Serial.println();
+  Serial.print("Connecting WiFi to: ");
+  Serial.println(ssid);
+  
+  // connect to network
+  WiFi.begin(ssid, password);
+  
+  // check status loop
+  while (WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+
+  // After connected into network
+  Serial.println();
+  Serial.println("WiFi connected");
+  
+  // Show device IP
+  Serial.print("Local IP: ");
+  Serial.println(WiFi.localIP());
+}
